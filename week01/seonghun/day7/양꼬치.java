6b214ed961799c6618f3876cccd9a2bc6711f662
seonghun <shp04078@naver.com>
2026-04-21 10:38:40 +0900

    feat: week01 4일차 풀이 완료

---
+++ week01/seonghun/day7/양꼬치.java
@@ -0,0 +1,11 @@
+package day7;
+
+public class 양꼬치 {
+    public int solution(int n, int k) {
+        int answer = 0;
+        int food = n * 12000;
+        int drink = (k - n / 10) * 2000;
+        answer = food + drink;
+        return answer;
+    }
+}
---
+++ week01/seonghun/day9/구슬을나누는경우의수.java
@@ -0,0 +1,16 @@
+package day9;
+
+public class 구슬을나누는경우의수 {
+    public int solution(int balls, int share) {
+        long answer = 1;
+
+        if(balls == share) return 1;
+
+        for(int i = 0 ; i < share ; i++) {
+            answer *= balls - i;
+            answer /= i + 1;
+        }
+
+        return (int)answer;
+    }
+}
