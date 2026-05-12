4bcd6615a350b61e8be4eed25acdde0a5978554e
Suhyun Cho <122531178+SOOsuhyuncho@users.noreply.github.com>
2026-05-12 13:35:27 +0900

    Revert "Minjeong"

--- lecture01/day4-class/src/object/Test.java
+++
@@ -1,7 +0,0 @@
-package object;
-
-public class Test {
-    public static void main(String[] args) {
-
-    }
-}
--- lecture02/src/main/java/ch11_lambda/basic/ch16/sec03/Speakable.java
+++
@@ -1,4 +0,0 @@
-package main.java.ch11_lambda.basic.ch16.sec03;
-
-public class Speakable {
-}
