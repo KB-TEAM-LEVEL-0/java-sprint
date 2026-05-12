07f00a308c7a7ab1b811fee4dd518d1aeae9c08b
jinseo <acgrp103@gmail.com>
2026-05-12 13:55:27 +0900

    진짜 8번까지 제대로 올림

---
+++ week3/jinseo/연속된자연수의합구하기.java
@@ -0,0 +1,34 @@
+package week3.jinseo;
+
+import java.util.Scanner;
+import java.io.IOException;
+
+public class 연속된자연수의합구하기{
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int count = 1;
+        int start_index = 1;
+        int end_index = 1;
+        int sum = 1;
+
+        while(end_index != N){//지정한 숫자까지 도달했을때 스톱
+
+            if(sum == N){//연속된 수를 찾았을때
+                count++;
+                end_index++;
+                sum= sum+end_index;
+            } else if (sum > N) {//일치하는 수를 찾기 -> 크면 앞에 짜르기
+                sum = sum - start_index;
+                start_index++;
+            } else {//일치하는 수를 찾기 -> 작으면 뒤에 짜르기
+                end_index++;
+                sum = sum + end_index;
+            }
+
+        }
+        System.out.println(count);
+
+    }
+}
