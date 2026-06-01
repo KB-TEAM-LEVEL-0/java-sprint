be3cb5433a71b93f21c44b9b689cec8c95f5fcb1
3iron <3iron38@gmail.com>
2026-06-01 11:34:53 +0900

    feat: week5 유연근무제

--- week5/minjeong/유연근무제.java
+++ week5/minjeong/유연근무제.java
@@ -1,15 +1,51 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+class Solution {
+    /* 매개변수 */
+    // schedules : 직원 n명이 설정한 출근 희망 시각, 1차원 정수 배열
+    // timelogs : 직원들이 일주일 동안 출근한 시각, 2차원 정수 배열
+    // startday : 이벤트 시작 요일
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    /* return */
+    // answer : 상품을 받을 직원의 수
+    public int solution(int[] schedules, int[][] timelogs, int startday) {
+        int answer = 0;
+        // 직원 수 n : schedules.length
+
+        // 1. HHMM 형태를 분 단위로 변환
+        for(int i=0; i<schedules.length; i++) {
+            int time = schedules[i] / 100;
+            // 시간 -> 분 단위로 변경
+            int minute = (schedules[i] - time*100) + time * 60;
+            // 희망 시간 + 10분
+            minute += 10;
+
+            // 이 직원이 상품을 받을 수 있는지
+            boolean pass = true;
+
+            // 일주일
+            for(int day=0; day<7; day++) {
+                int realDay = (startday + day - 1) % 7 + 1;
+
+                // 3. 평일만 : 6,7은 pass
+                if(realDay == 6 || realDay == 7) {
+                    continue;
+                }else {
+                    // 2. 출근 시각 : 출근 희망 시각 + 10분
+                    for(int j=startday-1; j<timelogs[i].length; j++) {
+                        int time2 = timelogs[i][day] / 100;
+                        int minute2 = (timelogs[i][day] - time2*100) + time2 * 60;
+
+                        if(minute2>minute) {
+                            pass = false;
+                            break;
+                        }else {
+                        }
+                    }
+                }
+            }
+            if(pass){
+                answer++;
+            }
         }
+        return answer;
     }
 }
