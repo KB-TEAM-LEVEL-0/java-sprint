c94f32a278bdc771dc0da7b4c97f78178ae2ef68
3iron <3iron38@gmail.com>
2026-06-01 14:21:55 +0900

    feat week5 크레인인형뽑기 - 풀이 중

--- week5/minjeong/크레인인형뽑기.java
+++ week5/minjeong/크레인인형뽑기.java
@@ -1,15 +1,49 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.*;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+class Solution {
+    public int solution(int[][] board, int[] moves) {
+        // n * n 크기 정사각 격자
+        // 바구니 : stack?
+        // 같은 모양 인형 2개 쌓이면 -> 사라짐 (터짐)
+        // 인형이 없는 곳은 아무 일도 일어나지 않음
+
+        // board = [
+        //  [0,0,0,0,0],
+        //  [0,0,1,0,3],
+        //  [0,2,5,0,1],
+        //  [4,2,4,4,2],
+        //  [3,5,1,3,1]
+        // ]
+
+        int answer = 0;
+        Stack<Integer> stack = new Stack<>();
+
+        int before = 0;
+
+        for(int move : moves) {
+            // 1. 인형을 뽑는데 없으면(0)이면 다음 행으로
+            // move : 열
+            // 인형의 종류는 5개?
+            // j : 행
+            for(int j=0; j<5; j++) {
+                if(board[j][move]==0) {
+                    continue;
+                }else {
+                    if(before == board[j][move]) {
+                        // 이전 넘버와 같으면 저장하지 않고 pop
+                        stack.pop();
+                        answer+=2;
+                        board[j][move] = 0;
+                        board[j-1][move] = 0;
+                    }else {
+                        stack.push(board[j][move]);
+                        // 이전 번호 저장
+                        before = board[j][move];
+                    }
+                    System.out.print(stack);
+                }
+            }
         }
+        return answer;
     }
 }
